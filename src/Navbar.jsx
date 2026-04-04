49a654cf1dcadafd4b939e970ce17b058528d6ee
Palash-soni <work.palashsoni@gmail.com>
2026-04-04 11:24:14 +0530

    Updated CV File

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import Resume from "./assets/Palash_Resume.pdf";
+import Resume from "./assets/Palash_Soni_Resume.pdf";
 import "./Nav.css";
 
 const Navbar = ({ highlight, onHome, onProjects, onSkills, onContact }) => {
